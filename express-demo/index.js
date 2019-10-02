cca5be951c165a171cc4e886dd8fdc21e7bbe9b0
Kah919 <kahyap13@gmail.com>
2019-10-02 03:01:09 -0400

    testing postman post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('@hapi/joi');
 const express = require('express'); // loading express module 
 const app = express(); // represents our application
+
+app.use(express.json()); // adding a middleware for our app
 
 // we have access to different methods to the HTTP verb/method
 // app.get()
@@ -29,6 +32,34 @@ app.get('/api/courses/:id', (req, res) => {
     if(!course) res.status(404).send('The course with the given ID was not found');
     res.send(course)
 })
+
+app.post('/api/courses', (req, res) => {
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(req.body);
+    console.log(result)
+
+
+
+
+
+    // if(!req.body.name || req.body.name.length < 3) {
+    //     // 400 Bad Request
+    //     res.status(400).send('Name is required')
+    //     return;
+    // }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
 
 
 // PORT
